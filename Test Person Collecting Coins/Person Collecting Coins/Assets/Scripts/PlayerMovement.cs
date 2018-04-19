3cf0547c76fd70f384d207d3797ec8279a400a3d
Aaron Trafford <uenvymenow@gmail.com>
2018-04-19 14:14:06 -0400

    Player is now able to move around map

---
+++ Test Person Collecting Coins/Person Collecting Coins/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+	// Access the player Rigidbody
+	private Rigidbody rb;
+
+	// Placeholder for thrust in Fixed Update
+	private float speed = 10.0f;
+	private float rotationSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void FixedUpdate() 
+	{
+		float verticalMovement = Input.GetAxis ("Vertical") * speed;
+		float horizontalMovement = Input.GetAxis ("Horizontal") * rotationSpeed;
+
+		rb.AddForce (horizontalMovement, 0, verticalMovement);
+	}
+}
